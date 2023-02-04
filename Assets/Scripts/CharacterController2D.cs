899c3f644baca72dfb3ae4d41abcca8c4971f97c
Mikko Suhonen <miksuh.dev@gmail.com>
2023-02-04 18:19:19 +0200

    feat: depth text

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -31,7 +31,8 @@ public class CharacterController2D : MonoBehaviour
 
     private float zoomMin = 3f;
 
-    public TMP_Text scoreText;
+    public TMP_Text waterText;
+    public TMP_Text depthText;
 
     void Start()
     {
@@ -127,7 +128,8 @@ public class CharacterController2D : MonoBehaviour
         visitedPoints.Add(player.position);
       }
 
-      scoreText.text = "Water: " + (Mathf.Round(playerWidth * 100));
+      waterText.text = "Water: " + (Mathf.Round(playerWidth * 100));
+      depthText.text = "Depth: " + (Mathf.Abs(Mathf.Round(player.position.y)));
 
       if (playerWidth <= 0) {
         GameOver();
